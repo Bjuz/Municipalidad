77c3f9bce0a7455be65464009098519b205cedac
Bjuz <jeaustin.rdz@.com>
2022-10-13 23:05:35 -0600

    result add

--- dist/util.test.js
+++ dist/util.test.js
@@ -3,31 +3,31 @@ const { loginComplete} = require('./util');
 const { ForgetPassword} = require('./util');
 
 test("login ok",  async ()=>{
-    const text2 =  await loginComplete("test@gmail.com","123456789");
-    console.log(text2)
-    expect(text2).toBe("Logueado");
+    const result =  await loginComplete("test@gmail.com","123456789");
+    console.log(result)
+    expect(result).toBe("Logueado");
 });
 
 test("login wrong email",  async ()=>{
-    const text2 =  await loginComplete("tes@gmail.com","123456789");
-    console.log(text2)
-    expect(text2).toBe("Firebase: Error (auth/user-not-found).");
+    const result =  await loginComplete("tes@gmail.com","123456789");
+    console.log(result)
+    expect(result).toBe("Firebase: Error (auth/user-not-found).");
 });
 
 test("login wrong password",  async ()=>{
-    const text2 =  await loginComplete("test@gmail.com","12345678");
-    console.log(text2)
-    expect(text2).toBe("Firebase: Error (auth/wrong-password).");
+    const result =  await loginComplete("test@gmail.com","12345678");
+    console.log(result)
+    expect(result).toBe("Firebase: Error (auth/wrong-password).");
 });
 
 test("forgot password Okay",  async ()=>{
-    const text2 =  await ForgetPassword("test@gmail.com");
-    console.log(text2)
-    expect(text2).toBe("Se ha enviado un correo para restablecer la contraseña");
+    const result =  await ForgetPassword("test@gmail.com");
+    console.log(result)
+    expect(result).toBe("Se ha enviado un correo para restablecer la contraseña");
 });
 
 test("forgot password Okay",  async ()=>{
-    const text2 =  await ForgetPassword("tes@gmail.com");
-    console.log(text2)
-    expect(text2).toBe("Firebase: Error (auth/user-not-found).");
+    const result =  await ForgetPassword("tes@gmail.com");
+    console.log(result)
+    expect(result).toBe("Firebase: Error (auth/user-not-found).");
 });
