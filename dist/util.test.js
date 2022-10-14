a810448c2d79bc9a83d09f84d40f0d44877bb077
Bjuz <jeaustin.rdz@.com>
2022-10-13 23:06:47 -0600

    result add notes

--- dist/util.test.js
+++ dist/util.test.js
@@ -3,31 +3,31 @@ const { loginComplete} = require('./util');
 const { ForgetPassword} = require('./util');
 
 test("login ok",  async ()=>{
-    const result =  await loginComplete("test@gmail.com","123456789");
+    const result =  await loginComplete("test@gmail.com","123456789"); // todo bien
     console.log(result)
     expect(result).toBe("Logueado");
 });
 
 test("login wrong email",  async ()=>{
-    const result =  await loginComplete("tes@gmail.com","123456789");
+    const result =  await loginComplete("tes@gmail.com","123456789");//falt la t
     console.log(result)
     expect(result).toBe("Firebase: Error (auth/user-not-found).");
 });
 
 test("login wrong password",  async ()=>{
-    const result =  await loginComplete("test@gmail.com","12345678");
+    const result =  await loginComplete("test@gmail.com","12345678");//falta el 9 en password
     console.log(result)
     expect(result).toBe("Firebase: Error (auth/wrong-password).");
 });
 
 test("forgot password Okay",  async ()=>{
-    const result =  await ForgetPassword("test@gmail.com");
+    const result =  await ForgetPassword("test@gmail.com");//todo bien
     console.log(result)
     expect(result).toBe("Se ha enviado un correo para restablecer la contraseña");
 });
 
 test("forgot password Okay",  async ()=>{
-    const result =  await ForgetPassword("tes@gmail.com");
+    const result =  await ForgetPassword("tes@gmail.com");//falta la t
     console.log(result)
     expect(result).toBe("Firebase: Error (auth/user-not-found).");
 });
