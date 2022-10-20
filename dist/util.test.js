dc32a22469efc90c09c4aa86a9939fd0081fc6c8
Bjuz <jeaustin.rdz@.com>
2022-10-19 18:22:27 -0600

    update email

--- dist/util.test.js
+++ dist/util.test.js
@@ -3,31 +3,31 @@ const { loginComplete} = require('./util');
 const { ForgetPassword} = require('./util');
 
 test("login ok",  async ()=>{
-    const result =  await loginComplete("test@gmail.com","123456789"); // todo bien // correo valido
+    const result =  await loginComplete("ti@santabarbara.go.cr","Municip$ali123"); // todo bien // correo valido
     console.log(result)
     expect(result).toBe("Logueado");
 });
 
 test("login wrong email",  async ()=>{
-    const result =  await loginComplete("tes@gmail.com","123456789");//falt la t
+    const result =  await loginComplete("talento@santabarbara.go.cr","Talen$to25");//falt la t
     console.log(result)
     expect(result).toBe("Firebase: Error (auth/user-not-found).");
 });
 
 test("login wrong password",  async ()=>{
-    const result =  await loginComplete("test@gmail.com","12345678");//falta el 9 en password
+    const result =  await loginComplete("asistenterh@santabarbara.go.cr","Asis$ten8");//falta el 9 en password
     console.log(result)
     expect(result).toBe("Firebase: Error (auth/wrong-password).");
 });
 
 test("forgot password Okay",  async ()=>{
-    const result =  await ForgetPassword("test@gmail.com");//todo bien 2
+    const result =  await ForgetPassword("ti@santabarbara.go.cr");//todo bien 2
     console.log(result)
     expect(result).toBe("Se ha enviado un correo para restablecer la contraseña");
 });
 
-test("forgot password Okay",  async ()=>{
-    const result =  await ForgetPassword("tes@gmail.com");//falta la t
+test("forgot password no Okay",  async ()=>{
+    const result =  await ForgetPassword("talentohuman@santabarbara.go.cr");//falta la t
     console.log(result)
     expect(result).toBe("Firebase: Error (auth/user-not-found).");
 });
