14a97c3d9ff1e58dab52538f0c90a026fec2e3d8
Francisco Arguedas <farguedas99@gmail.com>
2023-03-02 14:44:06 -0600

    Validations of data completed

--- src/SolicitarVacaciones.js
+++ src/SolicitarVacaciones.js
@@ -1,32 +1,38 @@
-// Receives the data in the form of SolicitarVacaciones.html and store it locally in the browser using JQuery
 
-$(document).ready(function () {
-    $("#vacationForm").submit(function () {
-        var formData = {
-            "firstDate": $("#firstDate").val(),
-            "finishDate": $("#finishDate").val(),
-        }
-        localStorage.setItem("formData", JSON.stringify(formData));
-        console.log(formData);
-    });
-});
+document.getElementById("sendData").onclick = function () {
 
+    //Get values from the form on SolicitarVacaciones.html
+    var firstDate = document.getElementById("firstDate").value;
+    var finishDate = document.getElementById("finishDate").value;
 
-// Receives the data in the form of SolicitarVacaciones.html and store it locally in the browser using vanilla JS
-// Get the data from the form
-/*function getFormData() {
-
-    var form = document.getElementById("vacationForm");
-    function handleForm(event) { event.preventDefault(); }
-    form.addEventListener('submit', handleForm);
-
-    var formData = {
-        "firstDate": document.getElementById("fechaInicio").value,
-        "lastDate": document.getElementById("fechaFin").value,
+    //Check if the dates are valid
+    if (firstDate == "" || finishDate == "") {
+        alert("Debe ingresar ambas fechas");
+        return;
+    } else if (firstDate > finishDate || firstDate == finishDate) {
+        alert("La fecha de inicio no puede ser mayor o igual a la fecha de fin");
+        return;
     }
-    localStorage.setItem("formData", JSON.stringify(formData));
-    console.log(formData);
-}*/
+
+    //Ask for confirmation from the user showing the dates formatting them to dd of month formatting month in spanish
+    var firstDateFormatted = new Date(firstDate);
+    var finishDateFormatted = new Date(finishDate);
+
+    var confirmation = confirm("¿Está seguro que desea solicitar vacaciones desde el " + firstDateFormatted.getDate() + " de " + firstDateFormatted.toLocaleString('es-ES', { month: 'long' }) + " hasta el " + finishDateFormatted.getDate() + " de " + finishDateFormatted.toLocaleString('es-ES', { month: 'long' }) + "?");
+    if (!confirmation) {
+        return;
+    }
+
+    // Storage the dates in the local storage
+    localStorage.setItem("firstDate", firstDate);
+    localStorage.setItem("finishDate", finishDate);
+
+    console.log("firstDate: " + firstDate);
+    console.log("finishDate: " + finishDate);
+
+    alert("Vacaciones solicitadas con éxito");
+};
+
 
 
 
