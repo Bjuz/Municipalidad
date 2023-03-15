6ec961bdce00a2219e791cbd83780636a79d55d5
Francisco_Arguedas <farguedas99@gmail.com>
2023-03-14 23:37:05 -0600

    New design fuctional

--- src/IngresarDiaFeriado.js
+++ src/IngresarDiaFeriado.js
@@ -1,21 +1,28 @@
-const { RegisterFeriado } = require('./util');
+/*document.getElementById("sendData").onclick = function () {
+  RegisterHoliday();
+};
 
+async function RegisterHoliday() {
+  var ancient = document.getElementById("date").value;
+  const text = await RegisterFeriado(ancient);
+}
+*/
+
+const { RegisterFeriado } = require("./util");
 
 document.getElementById("sendData").onclick = function () {
   document.getElementById("loader").style.display = "block";
-    RegisterHoliday();
+  RegisterHoliday();
 };
 
-async function RegisterHoliday(){
-var ancient = document.getElementById("date").value;
-  const text =  await RegisterFeriado(ancient);
+async function RegisterHoliday() {
+  var ancient = document.getElementById("date").value;
+  const text = await RegisterFeriado(ancient);
 
   alert(text);
   document.getElementById("loader").style.display = "none";
-  }
+}
 
-  window.addEventListener('DOMContentLoaded', async (event) => {
-    document.getElementById("loader").style.display = "none";
-  
-  });
-  
+window.addEventListener("DOMContentLoaded", async (event) => {
+  document.getElementById("loader").style.display = "none";
+});
