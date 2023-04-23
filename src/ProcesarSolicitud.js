b01946a70f55a4233b92d59a3cb92c2a46419347
Francisco_Arguedas <farguedas99@gmail.com>
2023-04-23 15:17:26 -0600

    Procesar solicitud generated dinamically - 1.0

--- src/ProcesarSolicitud.js
+++ src/ProcesarSolicitud.js
@@ -6,24 +6,65 @@ window.addEventListener("DOMContentLoaded", async (event) => {
   // Load users
   const users = await LoadUsers();
 
-  // Some users has an internal array called 'VacacionesActivas'
+  // Call the tbody element called 'tableBody'
+  const tableBody = document.getElementById("tableBody");
+
+  /* // Creates a 'tr' element 
+    const tr = document.createElement("tr");
+    tableBody.appendChild(tr);
+
+    // Creates a 'td' element
+    const td = document.createElement("td");
+    tr.appendChild(td);
+    // Dummy data 
+    td.innerHTML = "Dummy data";*/
+
+  // Now, some users has an internal array called 'VacacionesActivas'
   // that contains the vacations that the user has requested
   // and are waiting for approval
 
-  // I need to verify if this user has any vacations waiting for approval
-
   // Verify if the user has this internal array
+  users.forEach((user) => {
+    if (user.hasOwnProperty("VacacionesActivas")) {
+      const tr = document.createElement("tr");
+      tableBody.appendChild(tr);
+      //const tdNumeroSolcitud = document.createElement("td");
+      const tdNombre = document.createElement("td");
+      const tdEstado = document.createElement("td");
+      const tdFechaInicio = document.createElement("td");
+      const tdFechaFin = document.createElement("td");
 
-  /*
-    foreach (user in users); {
-        if (user.hasOwnProperty("VacacionesActivas")) {
-        
-        // If the user has this internal array, I need to verify if it has any vacations waiting for approval
-        // Verify if the user has any vacations waiting for approval
-        foreach (vacation in user.VacacionesActivas); {
-            
-        }
-    
+      tr.appendChild(tdNombre);
+      tr.appendChild(tdEstado);
+      tr.appendChild(tdFechaInicio);
+      tr.appendChild(tdFechaFin);
+
+      const vacations = user.VacacionesActivas;
+      vacations.forEach((vacation) => {
+        tdNombre.textContent = user.name;
+        tdEstado.textContent = vacation.Estado;
+        tdFechaInicio.textContent = vacation.firstDate;
+        tdFechaFin.textContent = vacation.LastDate;
+      });
     }
-    }*/
+
+    /*if (user.hasOwnProperty("VacacionesActivas")) {
+      // If the user has this internal array, I need to verify if it has any vacations waiting for approval
+      // Verify if the user has any vacations waiting for approval
+      const tr = document.createElement("tr");
+      tableBody.appendChild(tr);
+      //const tdNumeroSolcitud = document.createElement("td");
+      const tdNombre = document.createElement("td");
+      const tdEstado = document.createElement("td");
+      const tdFechaInicio = document.createElement("td");
+      const tdFechaFin = document.createElement("td");
+
+      foreach(vacation in user.VacacionesActivas);
+      {
+        tdNombre.innerHTML(user.Nombre);
+        tdEstado.innerHTML(vacation.Estado);
+        tdFechaInicio.innerHTML(vacation.firtDate);
+        tdFechaFin.innerHTML(vacation.LastDate);
+      }*/
+  });
 });
