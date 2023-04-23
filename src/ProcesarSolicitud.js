b9758fe778e77074ceb0c126833abb9d663b0944
Francisco Arguedas <farguedas99@gmail.com>
2023-04-23 17:34:19 -0600

    Editar solicitud re-added

--- src/ProcesarSolicitud.js
+++ src/ProcesarSolicitud.js
@@ -1,5 +1,7 @@
 const { LoadUsers } = require("./util");
 
+
+// Load users data and generates the table
 window.addEventListener("DOMContentLoaded", async (event) => {
   console.log("DOM fully loaded and parsed");
 
@@ -8,10 +10,12 @@ window.addEventListener("DOMContentLoaded", async (event) => {
 
   // Call the tbody element called 'tableBody'
   const tableBody = document.getElementById("tableBody");
-
   // Verify if the user has this internal array
+  var i = 0;
+
   users.forEach((user) => { 
     if (user.hasOwnProperty("VacacionesActivas")) {
+
       // I need to add a row for each vacation
       const vacations = user.VacacionesActivas;
       vacations.forEach((vacation) => {
@@ -24,61 +28,51 @@ window.addEventListener("DOMContentLoaded", async (event) => {
         const tdEstado = document.createElement("td");
         const tdFechaInicio = document.createElement("td");
         const tdFechaFin = document.createElement("td");
+
+        //Finally add to buttons to the row in the last column
+        const tdButtons = document.createElement("td");
+        const btnAprobar = document.createElement("button");
+        const btnRechazar = document.createElement("button");
+
+        // Also, this buttons need to have an id, the 'i' variable will help us to identify the row
+        btnAprobar.id = "btnAprobar" + i;
+        btnRechazar.id = "btnRechazar" + i;
+
+        btnAprobar.textContent = "Aprobar";
+        btnRechazar.textContent = "Rechazar";
+
+        tdButtons.appendChild(btnAprobar);
+        tdButtons.appendChild(btnRechazar);
+
         // Add the columns to the row
         //tr.appendChild(tdNumeroSolcitud);
         tr.appendChild(tdNombre);
         tr.appendChild(tdEstado);
         tr.appendChild(tdFechaInicio);
         tr.appendChild(tdFechaFin);
+        tr.appendChild(tdButtons);
         // Add the data to the columns
         //tdNumeroSolcitud.textContent = vacation.id;
         tdNombre.textContent = user.name;
         tdEstado.textContent = vacation.Estado;
         tdFechaInicio.textContent = vacation.firstDate;
         tdFechaFin.textContent = vacation.LastDate;
-      });
-      
+      }); 
     }
-
-    
   });
 });
 
 
-/* const tr = document.createElement("tr");
-      tableBody.appendChild(tr);
-      //const tdNumeroSolcitud = document.createElement("td");
-      const tdNombre = document.createElement("td");
-      const tdEstado = document.createElement("td");
-      const tdFechaInicio = document.createElement("td");
-      const tdFechaFin = document.createElement("td");
-
-      tr.appendChild(tdNombre);
-      tr.appendChild(tdEstado);
-      tr.appendChild(tdFechaInicio);
-      tr.appendChild(tdFechaFin);
-
-      const vacations = user.VacacionesActivas;
-      vacations.forEach((vacation) => {
-        tdNombre.textContent = user.name;
-        tdEstado.textContent = vacation.Estado;
-        tdFechaInicio.textContent = vacation.firstDate;
-        tdFechaFin.textContent = vacation.LastDate;
-      }); */
-  
-
-  
-
   
 
 function getListOfVacations(users){
-  var nVacations = 0;
+  var vacations = [];
 
   users.forEach((user) => {
     if (user.hasOwnProperty("VacacionesActivas")) {
       const vacations = user.VacacionesActivas;
       vacations.forEach((vacation) => {
-        nVacations++;
+        vacations.push(vacation);
       });
     }
   });
