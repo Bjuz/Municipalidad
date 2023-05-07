b7be5f2c671d5f8a5637ce7659aac3bfb057b360
Francisco_Arguedas <farguedas99@gmail.com>
2023-05-06 18:26:07 -0600

    Fix: Javascript not being called correctly on RegistroFuncionario.js

--- src/RegistroFuncionario.js
+++ src/RegistroFuncionario.js
@@ -1,30 +1,27 @@
-//const { RegisterUser } = require('./util');
-//const { ForgetPassword } = require("./util");
-//const { CreateANewUser } = require("./util");
+const { RegisterUser } = require("./util");
+const { ForgetPassword } = require("./util");
+const { CreateANewUser } = require("./util");
 
-
-//document.getElementById("sendData").onclick = function () {
- //// document.getElementById("loader").style.display = "block";
-  //Register();
-
-//};
+document.getElementById("sendData").onclick = function () {
+  //document.getElementById("loader").style.display = "block";
+  Register();
+};
 
 function generateP() {
-  var pass = '';
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
-          'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-    
+  var pass = "";
+  var str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+
   for (var i = 1; i <= 15; i++) {
-      var char = Math.floor(Math.random()
-                  * str.length + 1);
-        
-      pass += str.charAt(char)
+    var char = Math.floor(Math.random() * str.length + 1);
+
+    pass += str.charAt(char);
   }
-    
+
   return pass;
 }
 
-async function Register(){
+async function Register() {
   var id = document.getElementById("id").value;
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
@@ -37,34 +34,55 @@ async function Register(){
   var entryTime = document.getElementById("entryTime").value;
   var departureTime = document.getElementById("departureTime").value;
 
-  if(!id || !name||!email||!accumulatedDays||!ancient||!boss||!salary||!role||!entryTime||!departureTime){
+  if (
+    !id ||
+    !name ||
+    !email ||
+    !accumulatedDays ||
+    !ancient ||
+    !boss ||
+    !salary ||
+    !role ||
+    !entryTime ||
+    !departureTime
+  ) {
     alert("Favor agregar todos los campos");
-  }else{
-    const text =  await RegisterUser(id,name,email,accumulatedDays,ancient,boss,salary,role,entryTime,departureTime,generateP());
+  } else {
+    const text = await RegisterUser(
+      id,
+      name,
+      email,
+      accumulatedDays,
+      ancient,
+      boss,
+      salary,
+      role,
+      entryTime,
+      departureTime,
+      generateP()
+    );
     //const text =  await RegisterUser(id,name,email,accumulatedDays,ancient,boss,salary,role,entryTime,departureTime,generateP());
     var resultCreate;
     var resultForget;
-    if(text == "Usuario ingresado exitosamente"){
+    if (text == "Usuario ingresado exitosamente") {
       resultForget = await ForgetPassword(email);
     }
-  
+
     alert(resultForget);
-    var id = document.getElementById("id").value = "";
-    var name = document.getElementById("name").value = "";
-    var email = document.getElementById("email").value = "";
-    var accumulatedDays = document.getElementById("accumulatedDays").value = "";
-    var ancient = document.getElementById("ancient").value = "";
-    var boss = document.getElementById("boss").value = "";
-    var salary = document.getElementById("salary").value = "";
-    var role = document.getElementById("role").value = "";
-
+    var id = (document.getElementById("id").value = "");
+    var name = (document.getElementById("name").value = "");
+    var email = (document.getElementById("email").value = "");
+    var accumulatedDays = (document.getElementById("accumulatedDays").value =
+      "");
+    var ancient = (document.getElementById("ancient").value = "");
+    var boss = (document.getElementById("boss").value = "");
+    var salary = (document.getElementById("salary").value = "");
+    var role = (document.getElementById("role").value = "");
   }
- // document.getElementById("loader").style.display = "none";
+  // document.getElementById("loader").style.display = "none";
+}
 
- 
-  }
+//window.addEventListener('DOMContentLoaded', async (event) => {
+//document.getElementById("loader").style.display = "none";
 
-  //window.addEventListener('DOMContentLoaded', async (event) => {
-   //document.getElementById("loader").style.display = "none";
-  
- // });
+// });
