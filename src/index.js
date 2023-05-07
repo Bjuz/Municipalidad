8a7126dd0fd02426d0a84db2e333851f8ae195a0
Francisco_Arguedas <farguedas99@gmail.com>
2023-05-06 21:30:49 -0600

    Dinamic homepage depending on role

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { loginComplete } = require("./util");
 const { ForgetPassword } = require("./util");
 const { UserCurrentState } = require("./util");
+const { GetFuncionario} = require("./util");
 
 document.getElementById("Loggin").onclick = function () {
   document.getElementById("loader").style.display = "block";
@@ -17,7 +18,22 @@ async function loggin() {
   document.getElementById("loader").style.display = "none";
   if (text != "Correo o contraseña incorrecto.") {
     localStorage.setItem("userLoggueado", text);
-    location.href = "../html/Inicio.html";
+    
+    var user = await GetFuncionario(text);
+
+    // There are 3 types of users: Funcionario, Jefe Directo, Encargado de recursos humanos, Alcalde
+    if(user.role == "Funcionario"){
+      //window.location.href = "../html/InicioFuncionario.html";
+      window.location.href = "../html/Inicio.html";
+    }else if(user.role == "Jefe Directo"){
+      //window.location.href = "../html/InicioJefeDirecto.html";
+      window.location.href = "../html/Inicio.html";
+    }else if(user.role == "Encargado de recursos humanos"){
+      window.location.href = "../role/RecursosHumanos/InicioRH.html";
+    }else if(user.role == "Alcalde"){
+      //window.location.href = "../role/Alcalde/InicioAlcalde.html";
+      window.location.href = "../html/Inicio.html";
+    }
   } else {
     alert(text);
   }
