c5d7779cee963f37fecef94af7f5e1a682771c57
Francisco_Arguedas <farguedas99@gmail.com>
2023-05-08 01:25:08 -0600

    Inicio.html and Inicio.js role interface logic

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,3 +1,4 @@
+const { doc } = require("firebase/firestore");
 const { GetFuncionario } = require("./util");
 
 // On window load
@@ -12,11 +13,24 @@ window.onload = async function () {
     if (funcionario.role == "Alcalde" || funcionario.role == "Jefe directo") {
         // Hide the <a> element with id="registerLink"
         document.getElementById("registerLink").style.display = "none";
+        document.getElementById("registerLink").style.visibility = "hidden";
+
+        // Hide the <a> element with id="editLink"
+        document.getElementById("editLink").style.display = "none";
+        document.getElementById("editLink").style.visibility = "hidden";
+
+        // Hide the <a> element with id="deleteLink"
+        document.getElementById("deleteLink").style.display = "none";
+        document.getElementById("deleteLink").style.visibility = "hidden";
+
+        // Hide the <a> element with id="feriadoLink"
+        document.getElementById("feriadoLink").style.display = "none";
+        document.getElementById("feriadoLink").style.visibility = "hidden";
 
     } else if (funcionario.role == "Encargado de recursos humanos") {
         // Do nothing
     } else if (funcionario.role == "Funcionario") {
-        
+
     } else {
     }
 }
