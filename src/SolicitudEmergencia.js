56d9fad7572df2a92394914c8e7fafbbb482a77a
Francisco_Arguedas <farguedas99@gmail.com>
2023-05-13 20:36:45 -0600

    SolicitudEmergencia validation fix

--- src/SolicitudEmergencia.js
+++ src/SolicitudEmergencia.js
@@ -17,33 +17,43 @@ document.getElementById("sendData").onclick = function () {
     var finishDate = document.getElementById("finishDate").value;
     var motivo = document.getElementById("motivo").value;
 
-    //Check if the dates are valid
-    if (firstDate == "" || finishDate == "") {
-        alert("Debe ingresar ambas fechas");
-        return;
-    } else if (firstDate > finishDate || firstDate == finishDate) {
-        alert("La fecha de inicio no puede ser mayor o igual a la fecha de fin");
-        return;
-    }
-
     //Ask for confirmation from the user showing the dates formatting them to dd of month formatting month in spanish
     var firstDateFormatted = new Date(firstDate);
     var finishDateFormatted = new Date(finishDate);
+
+    if (!validateDates(firstDate, finishDate, motivo)) {
+        return;
+    }
 
     var confirmation = confirm("¿Está seguro que desea solicitar vacaciones desde el " + firstDateFormatted.getDate() + " de " + firstDateFormatted.toLocaleString('es-ES', { month: 'long' }) + " hasta el " + finishDateFormatted.getDate() + " de " + finishDateFormatted.toLocaleString('es-ES', { month: 'long' }) + "?");
     if (!confirmation) {
         return;
     }
 
-    // Storage the dates in the local storage
-    localStorage.setItem("firstDate", firstDate);
-    localStorage.setItem("finishDate", finishDate);
-    localStorage.setItem("motivo", motivo);
-
-    console.log("firstDate: " + firstDate);
-    console.log("finishDate: " + finishDate);
-    console.log("motivo: " + motivo);
+    //There is no any database functionality yet, so we just show an alert
 
     alert("Vacaciones solicitadas con alta prioridad");
 
+}
+
+// Function to validate the dates
+function validateDates(firstDate, finishDate, motivo) {
+    //Check if the dates are valid
+    if (firstDate == "" || finishDate == "") {
+        alert("Debe ingresar ambas fechas");
+        return false;
+    } else if (firstDate > finishDate || firstDate == finishDate) {
+        alert("La fecha de inicio no puede ser mayor o igual a la fecha de fin");
+        return false;
+    } // Check if the first date is greater than the current date
+    else if (new Date(firstDate) < new Date()) {
+        alert("La fecha de inicio no puede ser menor o igual a la fecha actual");
+        return false;
+    }
+    // Check if the reason is empty
+    else if (motivo == "") {
+        alert("Debe ingresar un motivo");
+        return false;
+    }
+    return true;
 }
