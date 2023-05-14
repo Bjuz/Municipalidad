3e04dbc06583cefe2c6a3cb91bf2a1b3edd8d502
Francisco_Arguedas <farguedas99@gmail.com>
2023-05-13 19:23:02 -0600

    DetalleVacaciones fixed

--- src/DetallesVacaciones.js
+++ src/DetallesVacaciones.js
@@ -30,6 +30,16 @@ window.addEventListener("DOMContentLoaded", async (event) => {
     var i = 0;
 
     CantVacaciones.innerHTML = " Cantidad de vacaciones actuales: " + CantidadDeVacaciones;
+
+    // Create headers for the table
+    Listas.innerHTML = `<tr>
+    <th>ID</th>
+    <th>Fecha de inicio</th>
+    <th>Fecha de finalizacion</th>
+    <th>Estado</th>
+    <th>Razon</th>
+    </tr>`;
+
     Vacations.forEach((element) => {
       i += 1;
       Listas.innerHTML += `<tr>
@@ -37,28 +47,31 @@ window.addEventListener("DOMContentLoaded", async (event) => {
       <td>${element.firstDate}</td>
       <td>${element.LastDate}</td>
       <td>${element.Estado}</td>
+      <td>${element.razon}</td>
      
       </tr>`
     })
 
+    document.getElementById("download").innerHTML = `<button class="btn btn-primary" onclick="download()">Descargar</button>`
+
   }
-
-
 
   // This function is called when the user clicks on the download button
   // It creates a xlsx file with the vacations and downloads it
   // Since the button was created with innerHTML, we need to use the window object
   // to access the function
   window.download = function () {
+    // Create a new workbook and a new worksheet
     var wb = XLSX.utils.book_new();
-    var ws = XLSX.utils.json_to_sheet(Vacations);
-    // Change the column name 'firstDate' to 'Fecha de inicio'
-    ws.A1.v = "Fecha de inicio";
-    // Change the column name 'LastDate' to 'Fecha de finalizacion'
-    ws.B1.v = "Fecha final";
+    var ws = XLSX.utils.table_to_sheet(document.getElementById("Listas"));
 
+    // Add the worksheet to the workbook
     XLSX.utils.book_append_sheet(wb, ws, "Vacaciones");
+
+    // Save the workbook as a xlsx file
     XLSX.writeFile(wb, "Vacaciones.xlsx");
+
+
   };
 
 });
