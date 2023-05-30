2e9f4cd91718585597dcdc415464117c76b10386
Francisco_Arguedas <farguedas99@gmail.com>
2023-05-30 00:07:52 -0600

    Add member first name to the welcome page

--- src/Inicio.js
+++ src/Inicio.js
@@ -8,5 +8,13 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   var funcionario = await GetFuncionario(userId);
 
+  var nameDiv = document.getElementById("nameDiv");
+
+  //Since this name includes the last name, we split it and only get the first name
+  var name = funcionario.name.split(" ");
+  var firstName = name[0];
+
+  nameDiv.innerHTML = "Bienvenido/a " + firstName + ", al Sistema de Gestión de Vacaciones.";
+
   roleDisplay(funcionario.role);
 });
